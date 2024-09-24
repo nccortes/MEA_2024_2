6e74bfa3e30769f41412330e8910a7afd4e83a3f
nccortes <nccortes@uc.cl>
2024-09-24 00:31:49 -0300

    Changed some writing on the readme and texts on tarea_2.ipynb. Also, changed some stuff on P4_funciones.

--- Tarea 2/P4_funciones.py
+++ Tarea 2/P4_funciones.py
@@ -3,36 +3,50 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
+#------------------------------------------------------------------------------------#
+# the first site is 0, insead of 1 as in classes.
+def Nbr_periodic(L):
+    N = L * L
+    Nbr_dict = {i : ((i // L) * L + (i + 1) % L, (i + L) % N,
+                     (i // L) * L + (i - 1) % L, (i - L) % N)
+                     for i in range(N)}
+    return Nbr_dict
 #------------------------------------------------------------------------#
 def energy_ising(S, Nbr_dict):
     E = 0
     for i in range(len(S)):
         for j in Nbr_dict[i]:
-            E -= S[i] * S[j]  # Each interaction is counted twice
-    return E / 2  # Correct for double counting
+            E -= S[i] * S[j]
+    return E / 2
+#------------------------------------------------------------------------#
+def calculate_binder_cumulant(magnetizations):
+    M2 = np.mean(np.square(magnetizations))
+    M4 = np.mean(np.power(magnetizations, 4))
+    U_L = (3 - (M4 / (M2**2))) / 2
+    return U_L
 #------------------------------------------------------------------------#
 def cluster_ising(L, T_values):
     N = L * L
-    # generación del directorio de vecinos
-    Nbr_dict = {i : ((i // L) * L + (i + 1) % L, (i + L) % N,
-                     (i // L) * L + (i - 1) % L, (i - L) % N)
-                     for i in range(N)}
-    # lista de spins y magnetización como la suma de los spines
+    # Neighbor directory
+    Nbr_dict = Nbr_periodic(L)
+    # random spin list and number of spins to flip
     S = [random.choice([1, -1]) for _ in range(N)]
     N_spins_to_flip = N * 100000
     results = []
+    binder_cumulants = []
+    magnetization_histograms = []
 
     for T in T_values:
-        # def de variables para cada temperatura
+        # Definition of variables for each temperature
         p_magic  = 1.0 - np.exp(-2.0 / T)
-        M_tot_abs = 0.0
         E_tot = 0.0
         E_tot_sq = 0.0
         N_steps = 0
-    
+        magnetizations = []
+
         N_flipped_spins = 0
         while N_flipped_spins < N_spins_to_flip:
-            # definición de conjuntos iniciales para Pocket y Cluster
+            # def. of Pocket and Cluster
             j = random.randint(0, N - 1)
             P, C = [j], [j]
             while P != []:
@@ -45,17 +59,43 @@ def cluster_ising(L, T_values):
             for k in C:
                 S[k] *= -1
 
-            # actualización de la energía y magnetización
+            # actualization of the energy
             E = energy_ising(S, Nbr_dict) / N  # Normalize energy per spin
             E_tot += E
             E_tot_sq += E * E
+            M = sum(S) / N  # Magnetization per spin
+            magnetizations.append(M)
             N_flipped_spins += len(C)
             N_steps += 1
-
-        # calcular los valores medios
-        E_avg = E_tot / N_steps  # <E> por spin
-        E_avg_sq = E_tot_sq / N_steps  # <E^2> por spin
+        
+        E_avg = E_tot / N_steps  # <E> per spin
+        E_avg_sq = E_tot_sq / N_steps  # <E^2> per spin
         c_V = (E_avg_sq - E_avg**2) * N / (T**2)
 
-        results.append((T, E_avg, c_V)) 
-    return results
+        magnetization_histograms.append(magnetizations)
+        U_L = calculate_binder_cumulant(magnetizations)
+        binder_cumulants.append(U_L)
+
+        results.append((T, E_avg, c_V))
+    return results, magnetization_histograms, binder_cumulants
+#------------------------------------------------------------------------#
+# Plotting function for magnetization histograms and Binder cumulants
+def plot_results(T_values, magnetization_histograms, binder_cumulants):
+    # Plot magnetization histograms
+    plt.figure(figsize=(10, 5))
+    for i, T in enumerate(T_values):
+        plt.hist(magnetization_histograms[i], bins=50, alpha=0.6, label=f"T = {T:.1f}")
+    plt.xlabel("Magnetization per spin")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.title("Magnetization Histograms")
+    plt.show()
+
+    # Plot Binder cumulant vs temperature
+    plt.figure()
+    plt.plot(T_values, binder_cumulants, 'o-')
+    plt.xlabel("Temperature")
+    plt.ylabel("Binder Cumulant")
+    plt.title("Binder Cumulant as a Function of Temperature")
+    plt.grid(True)
+    plt.show()
